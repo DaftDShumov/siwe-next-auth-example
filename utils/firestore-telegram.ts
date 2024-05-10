e1f8d1ab7265fdcdf95bb0f655dd3a634368c317
Dmytro Shumov <dmytro.shumov@daftmobile.com>
2024-05-10 21:56:26 +0200

    Ignore query for not started bot sessions

--- utils/firestore-telegram.ts
+++ utils/firestore-telegram.ts
@@ -7,6 +7,11 @@ interface SessionData {
 
 export async function updateSession(telegramId: string, address: string, token: string) {
   const sessions = firestore.collection("sessions")
+  const sessionSnapshot = await sessions.doc(telegramId).get()
+  if (!sessionSnapshot.exists) {
+    console.log('User by provided ID did not start the bot yet')
+    return
+  }
   const sessionData = (await sessions.doc(telegramId).get()).data() as SessionData
   sessionData.address = address
   sessionData.token = token
